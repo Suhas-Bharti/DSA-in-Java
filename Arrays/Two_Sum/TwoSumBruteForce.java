0588a4dcdcf15c894c2acb5c46ebd95dc6f39c93
Suhas-Bharti <suhasbharti2004@gmail.com>
2026-03-14 23:56:24 +0530

    Improve Two Sum brute force solution and fix comparison logic

--- Arrays/Two_Sum/TwoSumBruteForce.java
+++ Arrays/Two_Sum/TwoSumBruteForce.java
@@ -35,7 +35,7 @@ public class TwoSumBruteForce {
 
     public static int[] twoSum(int[] arr, int target) {
         for(int i=0; i<arr.length; i++){
-            for(int j=0; j<arr.length; j++) {
+            for(int j=i+1; j<arr.length; j++) {
                 if(arr[i] + arr[j] == target) {
                     return new int[]{i, j};
                 }
